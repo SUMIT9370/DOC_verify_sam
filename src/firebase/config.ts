ff96331de999db00747c43c323426ed534f0f35e
Sumit <sumitpatil9370@gmail.com>
2025-10-24 06:37:16 +0000

    pratik fack.png Verification failed.
    Uploading Securely
    Extracting Text

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9285548940-1ef81",
   "appId": "1:976876678503:web:32dd6449ab0f01b7bb53c2",
+  "storageBucket": "studio-9285548940-1ef81.appspot.com",
   "apiKey": "AIzaSyDMLxj4R-PikTzajQFQ-gerD8mOFE9q668",
   "authDomain": "studio-9285548940-1ef81.firebaseapp.com",
   "measurementId": "",
