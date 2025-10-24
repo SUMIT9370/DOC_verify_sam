cba032949cfd4c22fef1c7b292decbd72bdc6cce
Sumit <sumitpatil9370@gmail.com>
2025-10-24 04:59:15 +0000

    no i did'n see anything

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -53,7 +53,9 @@ export default function DashboardLayout({
   }
 
   const handleLogout = async () => {
-    await auth.signOut();
+    if (auth) {
+      await auth.signOut();
+    }
     router.push('/');
   };
 
